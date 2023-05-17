82ae44e9f21e54913c602c469b39906439fc747c
BruLekichLouro <bububio@yahoo.com.br>
2023-05-17 11:51:39 -0300

    Iniciando jogo

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    //fiunção chamada ao iniciar a execução do código;
+
+    createCanvas(1000, 600);
+}
+
+function Draw(){
+    //função chamada a cada frame(quadro)
+
+    background(220);//Limpa o pano de fundo e define sua cor
+    drawSprites();//Desenha os sprites na tela;
+}
+
